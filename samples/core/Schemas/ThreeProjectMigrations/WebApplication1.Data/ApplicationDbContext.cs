d4c75b8e855eaa5e48fb58f47d3fc15cf6d231f5
Dennis Dyallo <dennisdyallo@gmail.com>
2022-09-08 13:23:29 +0200

    Added Microsoft.EntityFrameworkCore.SqlServer and updates packages from 6.0.0 -> 6.0.8

--- samples/core/Schemas/ThreeProjectMigrations/WebApplication1.Data/ApplicationDbContext.cs
+++ samples/core/Schemas/ThreeProjectMigrations/WebApplication1.Data/ApplicationDbContext.cs
@@ -9,4 +9,12 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<TestModel> MainModel { get; set; }
+
+}
+
+public class TestModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
